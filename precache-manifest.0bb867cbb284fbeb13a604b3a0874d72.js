6b18805ed85a37aa9a95b5973d7fd90896283da1
Paras Patidar <patidarparas13@gmail.com>
2020-08-21 19:42:59 +0530

    Updates

---
+++ precache-manifest.0bb867cbb284fbeb13a604b3a0874d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac7d8fd763a7c6a52b4e9362d8ba43c",
+    "url": "/auth0-react/index.html"
+  },
+  {
+    "revision": "e0c17d22ac09411746be",
+    "url": "/auth0-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3cc9a9d7cc95f4e7392",
+    "url": "/auth0-react/static/js/2.c11dcad8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/auth0-react/static/js/2.c11dcad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c17d22ac09411746be",
+    "url": "/auth0-react/static/js/main.6696b28a.chunk.js"
+  },
+  {
+    "revision": "6d0b10aa52004207f9e2",
+    "url": "/auth0-react/static/js/runtime-main.4fe45b7f.js"
+  }
+]);
